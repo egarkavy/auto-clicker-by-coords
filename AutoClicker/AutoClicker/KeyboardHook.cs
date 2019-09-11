99acc14aaa6713bab051afc258a5200314feaea9
Egor <egarkavy@dev-team.com>
2019-09-11 17:01:41 +0300

    done

---
+++ AutoClicker/AutoClicker/KeyboardHook.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AutoClicker
+{
+    public delegate IntPtr KeyboardProcess(int nCode, IntPtr wParam, IntPtr lParam);
+
+    public sealed class KeyboardHook
+    {
+        public static event EventHandler<KeyPressedEventArgs> KeyPressed;
+        private const int WH_KEYBOARD = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static KeyboardProcess keyboardProc = HookCallback;
+        private static IntPtr hookID = IntPtr.Zero;
+
+        public static void CreateHook()
+        {
+            hookID = SetHook(keyboardProc);
+        }
+
+        public static void DisposeHook()
+        {
+            UnhookWindowsHookEx(hookID);
+        }
+
+        private static IntPtr SetHook(KeyboardProcess keyboardProc)
+        {
+            using (Process currentProcess = Process.GetCurrentProcess())
+            using (ProcessModule currentProcessModule = currentProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD, keyboardProc, GetModuleHandle(currentProcessModule.ModuleName), 0);
+            }
+        }
+
+        private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                if (KeyPressed != null)
+                    KeyPressed(null, new KeyPressedEventArgs(vkCode));
+            }
+            return CallNextHookEx(hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, KeyboardProcess lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+    }
+
+    public class KeyPressedEventArgs : EventArgs
+    {
+        public int KeyCode { get; set; }
+        public KeyPressedEventArgs(int Key)
+        {
+            KeyCode = Key;
+        }
+    }
+}
---
+++ AutoClicker/AutoClicker/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoClicker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int MOUSEEVENTF_MOVE = 0x0001; /* mouse move */
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002; /* left button down */
+        private const int MOUSEEVENTF_LEFTUP = 0x0004; /* left button up */
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008; /* right button down */
+
+        private POINT _savedCursorPosition;
+        private CancellationTokenSource _cancellationTokenSource { get; set; }
+        private Task _mauseJob;
+        private Task _keyboardJob;
+
+        private int ClickInterval { get; set; } = 1000;
+        private int KeyPressInterval { get; set; } = 1000;
+
+        private bool AllowDoJow { get; set; }
+
+        [DllImport("user32")]
+        public static extern int SetCursorPos(int x, int y);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+            
+            public static implicit operator Point(POINT point)
+            {
+                return new Point(point.X, point.Y);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention= CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            KeyboardHook.CreateHook();
+            KeyboardHook.KeyPressed += KeyboardHook_KeyPressed;
+        }
+
+
+        private void KeyboardHook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            if ((Keyboard.Modifiers == ModifierKeys.Control) && (e.KeyCode == 71))
+            {
+                ToggleWork();
+            }
+
+            if (!(Keyboard.Modifiers == ModifierKeys.Control) && (e.KeyCode == 71))
+            {
+                GetCursorPos(out _savedCursorPosition);
+            }
+        }
+
+        private void ToggleWork()
+        {
+            if(_mauseJob != null)
+            {
+                _cancellationTokenSource.Cancel();
+                _mauseJob = null;
+                _keyboardJob = null;
+            }
+            else
+            {
+                _cancellationTokenSource = new CancellationTokenSource();
+
+                AllowDoJow = true;
+
+                _mauseJob = MauseJob(_cancellationTokenSource.Token);
+                _keyboardJob = KeyboardJob(_cancellationTokenSource.Token);
+            }
+            
+        }
+
+        private async Task MauseJob(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested && AllowDoJow)
+            {
+                SetCursorPos(_savedCursorPosition.X, _savedCursorPosition.Y);
+                await Task.Delay(ClickInterval);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+            }
+        }
+
+        private async Task KeyboardJob(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested && AllowDoJow)
+            {
+                await Task.Delay(KeyPressInterval);
+                SendKeys.SendWait("{F5}");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ClickInterval = Int32.Parse(this.clickInterval.Text);
+            KeyPressInterval = Int32.Parse(this.keyPressInterval.Text);
+        }
+    }
+}
